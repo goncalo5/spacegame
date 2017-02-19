4b66907463a61575cda1994022e4f7ca91341725
goncalo5 <goncalo.s.and@gmail.com>
2017-02-19 07:44:24 +0000

    create building and resources files to separate them from logic file

--- logic.py
+++ logic.py
@@ -1,20 +1,21 @@
 import time
 import threading
-import database
 import constants
+from buildings import Building, Mine
+from resources import Resource
 
 
 class Logic(object):
     def __init__(self):
         # Resources
         # create resources's objects
-        self.metal = Resources('metal')
+        self.metal = Resource('metal')
         self.resources = [self.metal]
 
         # Buildings
         # create buildings's objects
-        self.metal_mine = Buildings('metal_mine')
-        self.robot_factory = Buildings('robot_factory')
+        self.metal_mine = Mine('metal_mine')
+        self.robot_factory = Building('robot_factory')
         self.buildings = [self.metal_mine, self.robot_factory]
         self.mines = [self.metal_mine]
 
@@ -57,84 +58,3 @@ class Logic(object):
                 return
             time.sleep(1)
             self.loop_evolve(building)
-
-
-class Resources(object):
-    def __init__(self, name):
-        self.name = name
-
-        # Null variables
-        self.total = None
-        self.per_s = None
-
-        # initial methods
-        self.see_total_in_db()
-        self.calculate_per_s()
-
-    # calculators
-    def calculate_per_s(self):
-        self.per_s = 1
-
-    # Data Base
-    # GET
-    def see_total_in_db(self):
-        self.total = database.RESOURCES[self.name]
-
-    # PUT
-    def save_total_in_db(self):
-        database.BUILDINGS[self.name]['total'] = self.total
-
-    def updating_total_in_db(self):
-        self.save_total_in_db()
-        time.sleep(constants.TIME2UPDATE_DB)
-        self.updating_total_in_db()
-
-
-class Buildings(object):
-    def __init__(self, name):
-        # initiate variables
-        self.name = name
-        # Null variables
-        self.level = None
-        self.cost = self.cost_lv0 = self.rate_cost = None
-        self.time_lv0 = self.rate_time = self.time = None
-        self.metal = None
-
-        # initial methods
-        self.see_level_in_db()
-        self.see_metal_in_db()
-
-        self.calculate_cost()
-        self.calculate_time2build()
-
-        #
-        self.evolving = False
-        self.left = self.time
-
-    # Data Base
-    # GET
-    def see_level_in_db(self):
-        self.level = database.BUILDINGS[self.name]['level']
-
-    def see_metal_in_db(self):
-        self.metal = database.RESOURCES['metal']
-
-    # calculators
-    def calculate_cost(self):
-        self.cost_lv0 = constants.BUILDINGS[self.name]['cost']
-        self.rate_cost = constants.BUILDINGS[self.name]['rate_cost']
-        self.cost = self.cost_lv0 * self.rate_cost**self.level
-
-    def calculate_time2build(self):
-        self.time_lv0 = constants.BUILDINGS[self.name]['time']
-        self.rate_time = constants.BUILDINGS[self.name]['rate_time']
-        self.time = self.time_lv0 * self.rate_time**self.level
-
-
-class Mines(Buildings):
-    def __init__(self, name):
-        super(Mines, self).__init__(name)
-        self.rate_per_s = None
-
-    def calculate_rate_per_s(self):
-        self.rate_per_s = 1.5
